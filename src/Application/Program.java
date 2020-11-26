ae1d489e62c5d41ac978582a33e829bb923a624f
Raphael Lages <raphael.lages@gmail.com>
2020-11-25 23:30:55 -0300

    Position class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,10 +1,17 @@
 package Application;
 
+import java.util.Scanner;
+
+import BoardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 	}
 
 }
